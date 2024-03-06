d3aa3f3a38685d7ce3b5397daceb5886355eb6d9
MuzammalAnsari <muzammalbash@gmail.com>
2024-03-06 17:34:58 +0500

    tweak

--- Node-Js/Client/src/Students/Students.jsx
+++ Node-Js/Client/src/Students/Students.jsx
@@ -162,7 +162,7 @@ function Students() {
         </div>
 
       </div>
-      <h2 className="text-xl font-bold mb-4">student Data</h2>
+      <h2 className="text-xl font-bold mb-4">Student Data</h2>
       <div className="grid grid-cols-7 gap-4">
         <div className="text-center font-bold">ID</div>
         <div className="text-center font-bold">First Name</div>
